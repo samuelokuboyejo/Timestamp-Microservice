0f444d263ad9aa3e24b73f3fb9817eaa5ad6f356
Samuel Okuboyejo <78604165+samuelokuboyejo@users.noreply.github.com>
2024-07-12 14:04:08 +0100

    Update TimestampController.java

--- Timestamp Microservice/Timestamp-Microservice/src/main/java/com/timestamp/Timestamp/Microservice/TimestampController.java
+++ Timestamp Microservice/Timestamp-Microservice/src/main/java/com/timestamp/Timestamp/Microservice/TimestampController.java
@@ -1,49 +1,31 @@
 package com.timestamp.Timestamp.Microservice;
 
-
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
-import java.time.Instant;
-import java.util.HashMap;
 import java.util.Map;
 
 @RestController
 public class TimestampController {
 
+    @Autowired
+    private TimestampService timestampService;
+
     @GetMapping("/{date}")
-   public Map<String, Object> getTimestamp(@PathVariable String date){
-       return generateTimestampResponse(date);
-
-   }
-
-    @GetMapping("/api/timestamp")
-    private Map<String, Object> generateTimestampResponse(String date) {
-        Map<String, Object> response = new HashMap<>();
-        Instant instant;
-
-        try{
-            if(date == null) {
-                instant = Instant.now();
-            }
-            else if (date.matches("\\d+")) {
-                instant = Instant.ofEpochMilli(Long.parseLong(date));}
-            else {
-                instant = Instant.parse(date);
-            }
-
-            response.put("unix", instant.toEpochMilli());
-            response.put("utc", instant.toString());
-        } catch (Exception e) {
-            response.put("error", "Invalid Date");
-        }
-
-
-        return response;
+    public Map<String, Object> getTimestamp(@PathVariable String date) {
+        return timestampService.generateTimestampResponse(date);
     }
 
-    public Map<String, Object> getCurrentTimestamp(){
-        return generateTimestampResponse(null);
+    @GetMapping("/api/timestamp")
+    public Map<String, Object> getTimestampTwo(@RequestParam(value = "date", required = false) String date) {
+        return timestampService.generateTimestampResponse(date);
+    }
+
+    @GetMapping("/api/timestamp/now")
+    public Map<String, Object> getCurrentTimestamp() {
+        return timestampService.generateTimestampResponse(null);
     }
 }
